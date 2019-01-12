03347cfe5856388eedb7ff8a38964f8ccd1b32a7
Sergey Petrin <spetrin@gmail.com>
2019-01-12 17:12:27 +0300

    SECRET_KEY reconfig for Travis

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+if os.getenv('BUILD_ON_TRAVIS', None):
+    SECRET_KEY = 'secret_key_for_travis'
+else:
+    SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
