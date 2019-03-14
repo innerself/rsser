3f4da4f43aa730940b485b4eb9fa2eac22dc6b26
Sergey Petrin <spetrin@gmail.com>
2019-03-14 11:44:27 +0300

    Add Sentry.io integration

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import sentry_sdk
 from decouple import config
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,6 +120,12 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# Sentry.io
+sentry_sdk.init(
+    dsn=config('SENTRY_DSN'),
+    integrations=[DjangoIntegration()]
+)
 
 
 # Static files (CSS, JavaScript, Images)
