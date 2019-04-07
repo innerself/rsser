afb40ec2726e2158f98221f65ee33bbb4a4864ff
Sergey Petrin <spetrin@gmail.com>
2019-04-07 16:00:23 +0300

    Add default ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -32,7 +32,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS', default="my.domain.ru")]
 
 
 # Application definition
