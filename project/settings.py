583459902b0f617d7da47d136a1585eeea1d86e1
Sergey Petrin <spetrin@gmail.com>
2019-07-01 22:35:32 +0300

    Fix email settings for TravisCI

--- project/settings.py
+++ project/settings.py
@@ -130,10 +130,10 @@ if config('SENTRY_RUN', cast=bool, default=False):
 
 
 # EMail
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.mydomain.com')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='my_user')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='my_password')
+EMAIL_PORT = config('EMAIL_PORT', default='25')
 
 
 # Static files (CSS, JavaScript, Images)
